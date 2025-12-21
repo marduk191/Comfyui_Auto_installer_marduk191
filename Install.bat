95ea1e50807ff11331b90f7b5f88c2cc6360b36c
marduk191 <marduk191@gmail.com>
2025-12-20 23:09:18 -0600

    added sparge attention

--- Install.bat
+++ Install.bat
@@ -14,6 +14,7 @@ pip install flash_attn-2.8.3+cu128torch2.9.0cxx11abiTRUE-cp312-cp312-win_amd64.w
 pip install insightface-0.7.3-cp312-cp312-win_amd64.whl
 pip install nunchaku-1.0.2+torch2.9-cp312-cp312-win_amd64.whl
 pip install sageattention-2.2.0+cu128torch2.9.0cxx11abi1-cp312-cp312-win_amd64.whl
+pip install spas_sage_attn-0.1.0+cu128torch2.9.0.post3-cp39-abi3-win_amd64.whl
 pip install torch==2.9.0 torchvision==0.24.0 torchaudio==2.9.0 --index-url https://download.pytorch.org/whl/cu128
 pip install numpy==1.26.4
 pause
